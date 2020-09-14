459396c3829eaccd3593076e326177d55519a910
Ivana Alvarez <iva.alvarez.2@gmail.com>
2020-09-14 18:33:31 -0400

    docs agregado

---
+++ docs/precache-manifest.9f86b0d4a2e7abf251751ac9aa20de01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43836c9da10fafc1e422cfa78ad7017b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1e0dd30776ea59d4013",
+    "url": "/static/css/main.6c8d7c40.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e0dd30776ea59d4013",
+    "url": "/static/js/main.341f6b5a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
